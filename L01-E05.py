eae9e03d0a4f45330ca7e2d8171f14b94d9c95d9
Francisco V <vxrnxk@gmail.com>
2020-05-11 09:00:07 -0300

    Exercícios 05 e 06

---
+++ L01-E05.py
@@ -0,0 +1,8 @@
+# Lista 01 - Exercício 05
+# Faça um Programa que converta metros para centímetros.
+
+medida_metros = float(input("Digite a medida em Metros: "))
+
+converte_metro_centimetros = medida_metros * 100
+
+print(f"{medida_metros} m equivalem a {converte_metro_centimetros} cm")
---
+++ L01-E06.py
@@ -0,0 +1,9 @@
+# # Lista 01 - Exercício 06
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+pi = 3.14
+raio = float(input("Digite o raio do círculo:"))
+
+area_circulo = pi * (raio * raio)
+
+print("A área do circulo é: {}".format(area_circulo))
